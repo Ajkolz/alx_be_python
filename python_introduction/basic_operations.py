8bbf3dd7ff96817fd40e9b9f02b18181ffc47f6d
Kolawole Ajayi <ajkolawole1993@gmail.com>
2024-06-23 12:35:15 -0700

    Added Basic operation file to Python introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# define the numbers
+
+
+number1 = 10
+number2 = 5
+
+# Perform addition, subtraction, and multiplication on these numbers.
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+
+# Print the results of these operations in a human-readable format.
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
