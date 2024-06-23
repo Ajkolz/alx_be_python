77598582a9baa0a21e9acef808ef91655a0566c7
Kolawole Ajayi <ajkolawole1993@gmail.com>
2024-06-23 15:24:27 -0700

    simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Define the variables
+
+Principal = 1000
+Rate = 0.05
+Time = 3
+
+# Calcute The Simple interest
+interest = Principal * Rate * Time
+
+#Print the Result
+print(f"The simple interest is: {interest}")
